065cda72204648a5830adc6f740151759e00e8f8
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-06-08 12:43:33 +0200

    replaced ft_str_count_chr with ft_split_count_str to avoid potential segfaults

--- src/parse/mrt_parse_color.c
+++ src/parse/mrt_parse_color.c
@@ -4,26 +4,24 @@ int	parse_color(const char *str, t_color *color)
 {
 	char	**split;
 	int		primary;
-	int		i;
 
-	if (ft_str_count_chr(str, ',') != 2)
-		return (-1);
 	split = ft_split(str, ',');
 	if (split == NULL)
 		return (-1);
-	i = 0;
-	while (i < 3)
+	if (ft_split_count_str(split) != 3)
 	{
-		if (int_from_str(split[i], 0, 255, &primary))
-			return (-1);
-		if (i == 0)
-			(*color).r = (double)primary / 255.0;
-		else if (i == 1)
-			(*color).g = (double)primary / 255.0;
-		else if (i == 2)
-			(*color).b = (double)primary / 255.0;
-		i++;
+		ft_free_split(&split);
+		return (-1);
 	}
+	if (int_from_str(split[0], 0, 255, &primary))
+		return (-1);
+	(*color).r = (double)primary / 255.0;
+	if (int_from_str(split[1], 0, 255, &primary))
+		return (-1);
+	(*color).g = (double)primary / 255.0;
+	if (int_from_str(split[2], 0, 255, &primary))
+		return (-1);
+	(*color).b = (double)primary / 255.0;
 	ft_free_split(&split);
 	return (0);
 }
--- src/parse/mrt_parse_vec3.c
+++ src/parse/mrt_parse_vec3.c
@@ -4,11 +4,14 @@ int	parse_vec3(const char *str, t_vec3 *vec3)
 {
 	char	**split;
 
-	if (ft_str_count_chr(str, ',') != 2)
-		return (-1);
 	split = ft_split(str, ',');
 	if (split == NULL)
 		return (-1);
+	if (ft_split_count_str(split) != 3)
+	{
+		ft_free_split(&split);
+		return (-1);
+	}
 	if (double_from_str(split[0], 6, 4, &((*vec3).x)))
 		return (-1);
 	if (double_from_str(split[1], 6, 4, &((*vec3).y)))
