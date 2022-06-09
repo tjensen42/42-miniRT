b1b861ff28c0afb175a51ad1f5b74c721c7e57f7
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-06-09 16:58:40 +0200

    fixed some bad ass leaks

--- src/parse/mrt_parse_color.c
+++ src/parse/mrt_parse_color.c
@@ -6,7 +6,7 @@
 /*   By: tjensen <tjensen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:56:52 by tjensen           #+#    #+#             */
-/*   Updated: 2022/06/09 14:56:53 by tjensen          ###   ########.fr       */
+/*   Updated: 2022/06/09 16:57:56 by tjensen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,18 @@ int	parse_color(const char *str, t_color *color)
 {
 	char	**split;
 	int		primary;
+	int		error;
 
 	split = ft_split(str, ',');
 	if (split == NULL)
 		return (-1);
-	if (ft_split_count_str(split) != 3)
-	{
-		ft_free_split(&split);
-		return (-1);
-	}
-	if (int_from_str(split[0], 0, 255, &primary))
-		return (-1);
+	error = (ft_split_count_str(split) != 3);
+	error = int_from_str(split[0], 0, 255, &primary);
 	(*color).r = (double)primary / 255.0;
-	if (int_from_str(split[1], 0, 255, &primary))
-		return (-1);
+	error = int_from_str(split[1], 0, 255, &primary);
 	(*color).g = (double)primary / 255.0;
-	if (int_from_str(split[2], 0, 255, &primary))
-		return (-1);
+	error = int_from_str(split[2], 0, 255, &primary);
 	(*color).b = (double)primary / 255.0;
 	ft_free_split(&split);
-	return (0);
+	return (error);
 }
--- src/parse/mrt_parse_vec3.c
+++ src/parse/mrt_parse_vec3.c
@@ -6,7 +6,7 @@
 /*   By: tjensen <tjensen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:56:19 by tjensen           #+#    #+#             */
-/*   Updated: 2022/06/09 14:56:19 by tjensen          ###   ########.fr       */
+/*   Updated: 2022/06/09 16:56:49 by tjensen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,16 @@
 
 int	parse_vec3(const char *str, t_vec3 *vec3)
 {
+	int		error;
 	char	**split;
 
 	split = ft_split(str, ',');
 	if (split == NULL)
 		return (-1);
-	if (ft_split_count_str(split) != 3)
-	{
-		ft_free_split(&split);
-		return (-1);
-	}
-	if (double_from_str(split[0], 6, 4, &((*vec3).x)))
-		return (-1);
-	if (double_from_str(split[1], 6, 4, &((*vec3).y)))
-		return (-1);
-	if (double_from_str(split[2], 6, 4, &((*vec3).z)))
-		return (-1);
+	error = (ft_split_count_str(split) != 3);
+	error = double_from_str(split[0], 6, 4, &((*vec3).x));
+	error = double_from_str(split[1], 6, 4, &((*vec3).y));
+	error = double_from_str(split[2], 6, 4, &((*vec3).z));
 	ft_free_split(&split);
-	return (0);
+	return (error);
 }
